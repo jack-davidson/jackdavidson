b62a74db95324b83cafd4fa0e07516be2d047d12
Jack Davidson <jackmaverickdavidson@gmail.com>
2020-11-23 13:13:04 -0600

    added core app for index page so I can separate the blog and the core so I can do easier development

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class BlogPost(models.Model):
+    title = models.CharField(max_length=50)
+    date = models.DateTimeField('date published')
+    
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
 def index(request):
-    return HttpResponse('you are at index')
+    return HttpResponse('you are at /blog')
+
+def blog(request, blogid):
+    return HttpResponse('you are at /blog/%i' % blogid)
