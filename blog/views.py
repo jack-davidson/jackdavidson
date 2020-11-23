6f211e37d6d55faa903a0c1ef8476ccd58b2c744
Jack Davidson <jackmaverickdavidson@gmail.com>
2020-11-23 13:34:26 -0600

    hacking around with views, will soon add templates

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import BlogPost
+
+# blog homepage
 def index(request):
     return HttpResponse('you are at /blog')
 
+# return most recently posted blog
 def blog(request, blogid):
-    return HttpResponse('you are at /blog/%i' % blogid)
+    return HttpResponse(BlogPost.objects.order_by('-date')[0]);
