85e779894eceb0f030e7b4ffe23ab2da9af7ef5e
Jack Davidson <jackmaverickdavidson@gmail.com>
2020-11-24 02:25:29 -0600

    did some model tinkering

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.utils.timezone import now
 
 class BlogPost(models.Model):
     title = models.CharField(max_length=50)
-    date = models.DateTimeField('date published')
+    date = models.DateTimeField(default=now, blank=True)
     document = models.CharField(max_length=10000, default="this document is blank")
     
     def __str__(self):
