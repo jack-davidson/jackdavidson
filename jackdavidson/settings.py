c0aa67652cc46b565b6f10e9fda025fc0f8edf59
Jack Davidson <jackmaverickdavidson@gmail.com>
2020-11-23 18:11:31 -0600

    fiddled with templates

--- jackdavidson/settings.py
+++ jackdavidson/settings.py
@@ -33,8 +33,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'core',
-    'admin',
     'blog',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -123,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
