78a2100226423d353360b7ecba8221682634f64f
Jack Davidson <jackmaverickdavidson@gmail.com>
2020-11-24 18:15:51 -0600

    added feature to preview the document on blog index

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 from django.utils.timezone import now
+import re
 
 class BlogPost(models.Model):
     title = models.CharField(max_length=50)
     date = models.DateTimeField(default=now, blank=True)
     document = models.TextField(max_length=10000, default="this document is blank")
+
+    # preview document. Returns first 50 characters with html tags removed.
+    def preview(self):
+        return re.compile(r'<[^>]+>').sub('', self.document[:50])
     
     def __str__(self):
         return self.title
