8798a3e7db17d591dd7ff3d8c9976205c6e20d58
J0ND03 <jackmaverickdavidson@gmail.com>
2021-01-14 19:11:51 -0600

    updates to formatting

--- src/blog/models.py
+++ src/blog/models.py
@@ -2,15 +2,17 @@ from django.db import models
 from django.utils.timezone import now
 import re
 
+
 class BlogPost(models.Model):
     title = models.CharField(max_length=50)
     date = models.DateTimeField(default=now, blank=False)
-    document = models.TextField(max_length=10000, default="this document is blank")
+    document = models.TextField(max_length=10000,
+                                default="this document is blank")
 
     # preview document. Returns first 50 characters with html tags removed.
     def preview(self):
         return re.compile(r'<[^>]+>').sub('', self.document[:50])
-    
+
     # instances of a BlogPost object can be identified by self.title
     def __str__(self):
         return self.title
